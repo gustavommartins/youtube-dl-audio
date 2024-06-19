bba1f76c363899d0891f5a25cbbd96343dad9d08
Gustavo Martins <gustavommartinsctg@outlook.com>
2024-06-19 01:36:32 -0300

    Feat: Save in directory based in your OS

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import platform
 
 try:
     from pytube import YouTube
@@ -6,7 +8,12 @@ try:
 except ImportError:
     # Se o Pytube não estiver instalado, instale-o automaticamente
     print("Pytube não encontrado. Instalando automaticamente...")
-    os.system(" pip install pytube==15.0.0 --user")
+    if platform.system() == "Linux":
+        os.system("sudo apt install pytube")
+    elif platform.system() == "Darwin":
+        os.system("brew install pytube")
+    elif platform.system() == "Windows":
+        os.system("pip install pytube==15.0.0 --user")
 
     # Importe o Pytube novamente após a instalação
     from pytube import YouTube
@@ -26,7 +33,22 @@ def baixar_musica(videoUrl, diretorio_destino="."):
         print(f"Ocorreu um erro ao baixar o seu aúdio: {e}")
 
 
+def get_download_directory():
+    """
+    Retorna o diretório de download padrão com base no sistema operacional.
+    """
+    if platform.system() == "Linux":
+        return os.path.join(os.path.expanduser("~"), "Música")
+    elif platform.system() == "Darwin":
+        return os.path.join(os.path.expanduser("~"), "Música")
+    elif platform.system() == "Windows":
+        return os.path.join(os.getenv("USERPROFILE"), "Downloads")
+    else:
+        # Retorna o diretório atual como padrão
+        return os.getcwd()
+
+
 if __name__ == "__main__":
     url = input("Digite a URL do vídeo do YouTube: ")
-    diretorio = "./audio_files"
+    diretorio = get_download_directory()
     baixar_musica(url, diretorio)
