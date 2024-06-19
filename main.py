5be8c096f7cd154fe9045ed44ed2e7b48e7497a4
Gustavo Martins <gustavommartinsctg@outlook.com>
2024-06-19 01:29:12 -0300

    Fix: AgeRestrictedError problem

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-import sys
+import os
 
 try:
     from pytube import YouTube
+    from pytube.innertube import _default_clients as client
 except ImportError:
-    print("Pytube não encontrado. Instale-o executando 'pip install pytube'")
-    sys.exit()
+    # Se o Pytube não estiver instalado, instale-o automaticamente
+    print("Pytube não encontrado. Instalando automaticamente...")
+    os.system(" pip install pytube==15.0.0 --user")
 
-def baixar_musica(url, diretorio_destino="."):
+    # Importe o Pytube novamente após a instalação
+    from pytube import YouTube
+    from pytube.innertube import _default_clients as client
+
+# Set a default client to AgeRestrictedError
+client["ANDROID_MUSIC"] = client["ANDROID_CREATOR"]
+
+
+def baixar_musica(videoUrl, diretorio_destino="."):
     try:
-        video = YouTube(url)
+        video = YouTube(videoUrl)
         stream = video.streams.filter(only_audio=True).first()
         stream.download(output_path=diretorio_destino)
-        print(f"O aúdio de '{video.title}' foi baixada com sucesso.")    
+        print(f"O aúdio de '{video.title}' foi baixada com sucesso.")
     except Exception as e:
         print(f"Ocorreu um erro ao baixar o seu aúdio: {e}")
+
 
 if __name__ == "__main__":
     url = input("Digite a URL do vídeo do YouTube: ")
